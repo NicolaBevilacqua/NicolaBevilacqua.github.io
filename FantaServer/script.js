78980cfaff95111de18fea6a4f33da12a0b314eb
NicolaBevilacqua <bevi02346@gmail.com>
2026-05-17 00:59:26 +0200

    aggiunta membro

--- FantaServer/script.js
+++ FantaServer/script.js
@@ -4,7 +4,7 @@ const utenti = [
     { nome:"Felis", ruolo:"Il tupperaio matto", punti:55, img:"Felis.jpg" },
     { nome:"Manel", ruolo:"il Musicofilo", punti:85, img:"manel.jpeg" },
     { nome:"Sosò", ruolo:"la voce della verità", punti:115, img:"Sofy.jpg" },
-    { nome:"Giulia", ruolo:" ", punti:145, img:"profilo.png" },
+    { nome:"La Madrina", ruolo:"Ma dici vero?!", punti:145, img:"Giulia.png" },
     { nome:"Diego", ruolo:" ", punti:105, img:"profilo.png" },
     { nome:"Alien", ruolo:" ", punti:40, img:"profilo.png" },
     { nome:"Giangiu", ruolo:" ", punti:65, img:"profilo.png" },
